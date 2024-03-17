837425706333c7bb5e2fb9bf263c597ccaf23482
Akash <pinnakaakash@gmail.com>
2024-03-17 11:21:41 -0400

    Added basic bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,28 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+provider "random" {
+  
+}
+
+
+resource "random_pet" "pet" {
+  length = 1
+}
+
+resource "aws_s3_bucket" "S3_bucket_EMR" {
+  bucket = "emr-athena-bucket-akash-${random_pet.pet.id}"
+
+
+  tags = {
+    Name        = "EMR-Athena-bucket-akashp"
+  }
+}
+
+resource "aws_s3_object" "fill_bucket" {
+  for_each = fileset("${path.module}/files", "**")
+
+  bucket = aws_s3_bucket.S3_bucket_EMR.id
+  key    = each.value // Ensure to include the trailing slash
+  source = "${path.module}/files/${each.value}"
+}
